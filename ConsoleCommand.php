4787eb63b891f8e2f5d50518c6ab436b9d902424
BigManzai <39168019+BigManzai@users.noreply.github.com>
2019-03-25 08:41:28 +0100

    V1.01

--- ConsoleCommand.php
+++ ConsoleCommand.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html><html><head><meta charset="utf-8">
 
 <!-- 
-	OpenSim Console Commands V1.00 by Manfred Aabye
+	OpenSim Console Commands V1.01 by Manfred Aabye
  -->
-<title>Console</title>
+<title>OS Console</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -16,7 +16,7 @@
 <!-- Kopfzeile -->
 	<div class="w3-bar w3-large w3-theme-d4">
 	  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-bars"></i></a>
-	  <span class="w3-bar-item">Console Commands</span>
+	  <span class="w3-bar-item">OpenSimulator Console Commands</span>
 	</div>
 
 <?php if (!isset($_POST['telefon'])): ?>
@@ -24,27 +24,27 @@
 <form class="w3-container" action="" method="post">
     <input type="hidden" name="telefon" value="1" />
 	
-<!-- Server Einstellung --> 
+<!-- OpenSim Einstellung --> 
 	<div class="w3-row w3-section">
 	<p><label class="w3-text"><i class="fa fa-cogs" style="font-size:48px;color:grey"></i></label></p>
 	
-    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24px"></i>  Server IP:</b></label></p>
+    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24px"></i>  OpenSim IP:</b></label></p>
         <div class="w3-third">
-            <p><input class="w3-input w3-border" type="text" placeholder="Server IP" name="Server_IP"/></p>
+            <p><input class="w3-input w3-border" type="text" placeholder="OpenSim IP" name="OpenSim_IP"/></p>
         </div>
     </div>
  
 	<div class="w3-row w3-section">
-    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24px"></i>  Server Port:</b></label></p>
+    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24px"></i>  OpenSim Port:</b></label></p>
         <div class="w3-third">
-            <p><input class="w3-input w3-border" type="text" placeholder="Server Port" name="Server_Port"/></p>
+            <p><input class="w3-input w3-border" type="text" placeholder="OpenSim Port" name="OpenSim_Port"/></p>
         </div>
     </div>
 
  	<div class="w3-row w3-section">
-    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24px"></i>  Server Password:</b></label></p>
+    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24px"></i>  OpenSim Password:</b></label></p>
         <div class="w3-third">
-            <p><input class="w3-input w3-border" type="password" placeholder="Server Password" name="Server_Password"/></p>
+            <p><input class="w3-input w3-border" type="password" placeholder="OpenSim Password" name="OpenSim_Password"/></p>
         </div>
     </div>
 	
@@ -53,9 +53,9 @@
  	<div class="w3-row w3-section">
 	<p><label class="w3-text"><i class="glyphicon glyphicon-file" style="font-size:48px;color:grey"></i></label></p>
 	
-    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24"></i>  Server Command:</b></label></p>
+    <p><label for="base" class="w3-label control-label"><i class="fa fa-pencil" style="font-size:24"></i>  OpenSim Command:</b></label></p>
         <div class="w3-third">
-            <input class="w3-input w3-border w3-light-grey" type="text" placeholder="Server Command" name="Server_Command"/>
+            <input class="w3-input w3-border w3-light-grey" type="text" placeholder="OpenSim Command" name="OpenSim_Command"/>
         </div>
     </div> 
 	
@@ -64,9 +64,10 @@
 <!-- Command Button -->
 	
     <div class="w3-row w3-section">
-
-            <button class="w3-btn w3-blue-grey w3-third" type="submit" name="submit"><i class="fa fa-mail-forward" style="font-size:24px"></i>  SendCommand</button>
-
+	<div class="w3-third">
+        <button class="w3-btn w3-blue-grey w3-half" type="submit" name="submit"><i class="fa fa-mail-forward" style="font-size:24px"></i>  Send OpenSim Command</button>
+		<button class="w3-btn w3-blue-grey w3-half" type="reset" name="Reset"><i class="fa fa-mail-reply" style="font-size:24px"></i>  Data Reset</button>
+	</div>
     </div>
 	
 </form>
@@ -76,25 +77,32 @@
 </div>
 
 <?php
-if (isset($_POST['telefon']) AND $_POST['telefon'] == 1)
+function senden() 
 {
-    // wir schaffen unsere Variablen und alle Leerzeichen beiläufig entfernen	
+	if (isset($_POST['telefon']) AND $_POST['telefon'] == 1)
+		{
+			// wir schaffen unsere Variablen und alle Leerzeichen beiläufig entfernen	
 
-	$Server_IP   = trim($_POST['Server_IP']);
-	$Server_Port   = trim($_POST['Server_Port']);
-    $Server_Password  = $_POST['Server_Password'];
-    $Server_Command   = trim($_POST['Server_Command']);
+			$OpenSim_IP   = trim($_POST['OpenSim_IP']);
+			$OpenSim_Port   = trim($_POST['OpenSim_Port']);
+			$OpenSim_Password  = $_POST['OpenSim_Password'];
+			$OpenSim_Command   = trim($_POST['OpenSim_Command']);
 
-	// Einlesen der RemoteAdmin Datei.
-	include('RemoteAdmin.php');
-	 
-	// Mit OpenSim verbinden
-	$myRemoteAdmin = new RemoteAdmin($Server_IP, $Server_Port, $Server_Password);
-	 
-	// OpenSim Konsolenbefehl samt parameter senden
-	$parameters = array('command' => $Server_Command);
-	$myRemoteAdmin->SendCommand('admin_console_command', $parameters);
+			// Einlesen der RemoteAdmin Datei.
+			include('RemoteAdmin.php');
+			 
+			// Mit OpenSim verbinden
+			$myRemoteAdmin = new RemoteAdmin($OpenSim_IP, $OpenSim_Port, $OpenSim_Password);
+			 
+			// OpenSim Konsolenbefehl samt parameter senden
+			$parameters = array('command' => $OpenSim_Command);
+			$myRemoteAdmin->SendCommand('admin_console_command', $parameters);
+			// Jetzt die Seite neu starten
+			?><meta http-equiv="refresh" content="0; URL=./ConsoleCommand.php"><?php 
+		}
 }
+// Die Funktion senden aufrufen
+senden();
 ?>
 
 </div>
